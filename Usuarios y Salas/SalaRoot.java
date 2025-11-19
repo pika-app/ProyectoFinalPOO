491b3f1199d54c54806692ae4716a5dc7e0aae26
pika-app <65641308+pika-app@users.noreply.github.com>
2025-11-19 03:08:31 -0600

    Update SalaRoot.java
    aun no esta acabada

--- Usuarios y Salas/SalaRoot.java
+++ Usuarios y Salas/SalaRoot.java
@@ -1,1 +1,29 @@
-//para tener una base
+import java.util.List;
+import java.util.LinkedList; 
+import java.util.Scanner; 
+
+public class SalaRoot {
+    private String nombreSala; 
+    private String contraseniaSala; 
+    private Usuario_root administradorSala;
+    private List<Usuario_n> usuariosInscritos;
+    private List<Nota> comunicados;
+    private List<Recompensa> recompensas; 
+    private List<Actividad> actividades;
+    private Scanner teclado;
+    
+    public SalaRoot(String nombreSala,String contraseniaSala, Usuario_root administradorSala){
+        this.nombreSala = nombreSala;
+        this.contraseniaSala = contraseniaSala;
+        this.administradorSala = administradorSala;
+        this.usuariosInscritos = new LinkedList<>();
+        this.comunicados = new LinkedList<>();
+        this.recompensas = new LinkedList<>();
+        this.actividades = new LinkedList<>();
+        this.teclado = new Scanner(System.in);
+    }
+    
+    public void verUsuarios(){
+        
+    }
+}
