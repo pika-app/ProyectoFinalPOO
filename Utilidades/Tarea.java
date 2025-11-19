dc70c84dc1304dfc5e52132f6298ebfcd5e8ac86
pika-app <65641308+pika-app@users.noreply.github.com>
2025-11-18 20:53:30 -0600

    Update Tarea.java
    lol xd, checalo bien recuerda que el diseño no siempre está bien

--- Utilidades/Tarea.java
+++ Utilidades/Tarea.java
@@ -1,1 +1,54 @@
-//para tener una base
+import java.time.LocalDate;
+
+public class Tarea {
+    private String nombre;
+    private double puntaje;
+    private String descripcion;
+    private boolean estado;
+    private LocalDate fechaEntrega;
+
+    public Tarea(String nombre, double puntaje, String descripcion, boolean estado, int anio, int mes, int dia) {
+        this.nombre = nombre;
+        this.puntaje = puntaje;
+        this.descripcion = descripcion;
+        this.estado = estado;
+        this.fechaEntrega = LocalDate.of(anio,mes,dia);
+    }
+
+public String getNombre() {
+    return nombre;
+}
+
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+
+public double getPuntaje() {
+    return puntaje;
+}
+
+public void setPuntaje(double puntaje) {
+    this.puntaje = puntaje;
+}
+
+public String getDescripcion() {
+    return descripcion;
+}
+
+public void setDescripcion(String descripcion) {
+    this.descripcion = descripcion;
+}
+
+public boolean getEstado() {
+    return estado;
+}
+
+public void setEstado(boolean estado) {
+    this.estado = estado;
+}
+
+public void mostrarFecha(){
+    System.out.println("La fecha de entrega es "+fechaEntrega.getDayOfYear()+"/"+fechaEntrega.getDayOfMonth()+"/"+fechaEntrega.getDayOfWeek());
+}
+
+}
